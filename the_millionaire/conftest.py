f9ea373dc6c94a7cadf926d259ce1de349fc100d
Amirhossein Saemi <saemiamirhossein@gmail.com>
2021-09-05 13:01:26 -0400

    Initialized the project with users and questions app

---
+++ the_millionaire/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from the_millionaire.users.models import User
+from the_millionaire.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
